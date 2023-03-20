2113c95e19dd85b483d05e0497e90697c6d57f25
JeanieV <venterjeaniebs@gmail.com>
2023-03-20 13:42:29 +0200

    Update #5
    Empty array list completed. Removed the local storage (will add in end)

--- src/js/application.js
+++ src/js/application.js
@@ -1,12 +1,51 @@
-//Query Selectors
-let inputTask = document.querySelector("#task");
-let submitTask = document.querySelector(".addTask");
-let tasksDiv = document.querySelector(".tasks");
-let deleteTask = document.querySelector(".deleteTask");
-let sortTask = document.querySelector(".sortTask");
-let editTask = document.querySelector(".editTask");
+//Get Element by class and id
+let inputTask = document.getElementById("task");
+let submitTask = document.getElementById("addTask1");
+let tasksDiv = document.getElementsByClassName("tasks");
+let deleteTask = document.getElementById("deleteTask1");
+let sortTask = document.getElementById("sortTask1");
+let editTask = document.getElementById("editTask1");
 
 //Empty Task Array to store input
-let taskArray =[];
+let taskArray = [];
 
 
+function submitFunc(event) {
+    event.preventDefault();
+
+    if (inputTask.value !== "") {
+        newTaskArray(inputTask.value);
+        inputTask.value = "";
+    }
+    else {
+        console.log("It did not work!")
+    }
+}
+
+submitTask.addEventListener("click", submitFunc);
+
+
+function newTaskArray(taskText) {
+
+    //Object created
+    const task = {
+        time: Date(),
+        title: taskText,
+        completed: false,
+    };
+
+    //Push the user input into empty array
+    taskArray.push(task);
+    console.log(taskArray);
+
+}
+
+function listOfTasks(){
+
+    let task1 = 
+    `
+    
+    `
+
+    return task1
+}
