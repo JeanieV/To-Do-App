ec4c074385f1ace910405afe8892f66174c8d2cb
JeanieV <venterjeaniebs@gmail.com>
2023-03-20 11:01:17 +0200

    Update #4
    Query Selectors added with empty array and images added on same line as input field

---
+++ src/js/application.js
@@ -0,0 +1,12 @@
+//Query Selectors
+let inputTask = document.querySelector("#task");
+let submitTask = document.querySelector(".addTask");
+let tasksDiv = document.querySelector(".tasks");
+let deleteTask = document.querySelector(".deleteTask");
+let sortTask = document.querySelector(".sortTask");
+let editTask = document.querySelector(".editTask");
+
+//Empty Task Array to store input
+let taskArray =[];
+
+
