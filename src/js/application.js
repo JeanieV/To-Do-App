0ac4f64bcb485cc99594f10bb517b3a06583058e
JeanieV <venterjeaniebs@gmail.com>
2023-03-21 13:04:04 +0200

    Update #7
    List appends by use of template literal. Functionality works with add not yet delete

--- src/js/application.js
+++ src/js/application.js
@@ -1,99 +1,108 @@
 //Get Element by class and id
-let inputTask = document.getElementById("task");
-let submitTask = document.getElementById("addTask1");
-let tasksDiv = document.getElementById("tasks")
-let deleteTask = document.getElementById("deleteTask1");
-let sortTask = document.getElementById("sortTask1");
-let editTask = document.getElementById("editTask1");
+const inputTask = document.getElementById("task");
+const submitTask = document.getElementById("addTask1");
+const deleteTask = document.getElementById("deleteTask1");
+const sortTask = document.getElementById("sortTask1");
+const editTask = document.getElementById("editTask1");
+const inputDate = document.getElementById("date")
 
-//Empty Task Array to store input
+//Where the list should go
+let newTasks = document.getElementById("tasks");
+
+// //Empty Task Array to store input
 let taskArray = [];
 
 
+//Function for what happens when user clicks on Add Task
 function submitFunc(event) {
     event.preventDefault();
+    
 
-    if (inputTask.value !== "") {
-        newTaskArray(inputTask.value);
-        inputTask.value = "";
+    let toDo = inputTask.value;
+    toDo = toDo.trim();
+
+    let toDate = inputDate.value;
+    toDate = toDate.trim();
+
+    if (toDo == "") {
+        alert("Kindly enter a new task!");
+    }
+    else if (toDate == "") {
+        alert("Kindly enter a new due date!")
+        
     }
     else {
-        console.log("It did not work!")
-    }
-}
+        console.log(taskArray);
+        newTaskArray(inputTask.value);
 
+        //This will append to form when the user enters task and due date
+        let task1 = 
+             `
+            <div class="spanLine">
+                <p class="span1"> ${inputTask.value} </p>
+
+                <p class="span2"> ${inputDate.value} </p>
+        
+                <button type="submit" id="editTask2"><img class="editTask" src="/src/images/edit.gif" alt="Edit Task" title="Edit Task"
+                        attribution="https://www.flaticon.com/free-animated-icons/paper"></button>
+
+                <button type="submit" id="deleteTask2"><img class="deleteTask" src="/src/images/bin.gif"
+                        alt="Delete Task" title="Delete Task"
+                        attribution="https://www.flaticon.com/free-animated-icons/document"></button>
+            </div>
+            `
+        document.getElementById("tasks").innerHTML += task1
+    }
+    
+}
 submitTask.addEventListener("click", submitFunc);
 
 
+// //This function will be added to the empty array
 function newTaskArray(taskText) {
 
     //Object created
     const task = {
-        time: Date(),
+        time: inputDate.value,
         title: taskText,
-        completed: false,
     };
 
     //Push the user input into empty array
     taskArray.push(task);
-    console.log(taskArray);
-    populateUserList(taskArray);
+    // console.log(taskArray);
 }
 
 
-function viewTask(index){
-
-    let task1 = 
-    `
-    <div class="taskLine" data-index="${index}">
-                <input type="text" placeholder="Enter your Task.." name="task" id="${inputTask}">
-                <button type="submit" id="${submitTask}"><img class="addTask" src="/src/images/add-file.gif" alt="Add Task" title="Add Task"
-                        attribution="https://www.flaticon.com/free-animated-icons/add-file"></button>
-                <button type="submit" id="${editTask}"><img class="editTask" src="/src/images/edit.gif" alt="Edit Task" title="Edit Task"
-                        attribution="https://www.flaticon.com/free-animated-icons/paper"></button>
-                <button type="submit" id="${sortTask}"><img class="sortTask" src="/src/images/sort.gif" alt="Sort Task" title="Sort Task"
-                        attribution="https://www.flaticon.com/free-animated-icons/arrows"></button>
-                <button type="submit" id="${deleteTask}"><img class="deleteTask" src="/src/images/bin.gif" alt="Delete Task" title="Delete Task"
-                        attribution="https://www.flaticon.com/free-animated-icons/document"></button>
-            </div>
-    `
-return task1;
-}
 
 
-function populateUserList(usersparam) {
-    let listElements = document.getElementsByClassName("taskLine");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Delete task from array
+// function deleteTaskArray() {
+
+//     let x = inputTask.value
     
-    Array.from(listElements).forEach((el) => {
-        el.remove();
-    })
-
-    for (let i = 0; i < taskArray.length; i++) {
-        
-        let user1 = viewTask(usersparam[i], i);
-        
-        document.getElementById("tasks").innerHTML += user1;
-        console.log(user1);
-    }
-
-    // Update data-index attributes
-    listElements = document.getElementsByClassName("taskLine");
-
-    Array.from(listElements).forEach((el, index) => {
-        el.setAttribute("data-index", index);
-        
-        deleteTask.addEventListener("click", (event) => {
-            removeUser(event, index);
-        });
-    });
-}
+//     taskArray.splice(x,1)
+//     console.log(taskArray);
+// }
+// deleteTask.addEventListener('click',deleteTaskArray);
 
 
-function removeUser(event, index) {
-    const entry = event.target.parentNode;
-    taskArray.splice(index, 1);
-    entry.parentNode.removeChild(entry);
-    populateUserList(taskArray);
-
-    console.log(taskArray);
-}
