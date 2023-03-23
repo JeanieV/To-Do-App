d888585fc64bb5cee8a3eef2f3e646e467483dfb
JeanieV <venterjeaniebs@gmail.com>
2023-03-23 19:06:17 +0200

    Update #13
    Delete button is now working and deleting the task out of the array

--- src/js/application.js
+++ src/js/application.js
@@ -1,14 +1,12 @@
 //Get Element by class and id
 const inputTask = document.getElementById("task");
 const submitTask = document.getElementById("addTask1");
-const deleteTask = document.getElementById("deleteTask2");
-const checkTask = document.getElementById("checkTask2");
 const sortTask = document.getElementById("sortTask1");
-const editTask = document.getElementById("editTask2");
-const inputDate = document.getElementById("date")
+const inputDate = document.getElementById("date");
 
-//Where the list should go
+//Where the tasklist should go
 let newTasks = document.getElementById("tasks");
+
 
 // //Empty Task Array to store input
 let taskArray = [];
@@ -25,7 +23,7 @@ function submitFunc(event) {
     let toDate = inputDate.value;
     toDate = toDate.trim();
 
-
+    //Form Validation
     if (toDo == "") {
         alert("Kindly enter a new task!");
     }
@@ -38,38 +36,43 @@ function submitFunc(event) {
         newTaskArray(inputTask.value);
 
         appendTask(taskArray);
-        
         taskOperations(taskArray);
-        
-}
+
+        // Clear the form inputs
+        document.getElementById("task").value = '';
+        document.getElementById("date").value = '';
+
+
+    }
 }
 submitTask.addEventListener("click", submitFunc);
 
 
 //This will append to form when the user enters task and due date
-function appendTask(task){
+function appendTask(task) {
 
     let task1 =
-            `
-            <div class="spanLine" >
+        `
+            <div class="spanLine">
                 <p class="span1"> ${task.title} </p>
 
                 <p class="span2"> ${task.time} </p>
         
-                <button type="submit" id="checkTask2"><img class="checkTask" src="/src/images/check.gif" alt="Completed" title="Completed"
+                <button type="submit" class="checkTask2"><img class="checkTask" src="/src/images/check.gif" alt="Completed" title="Completed"
                         attribution="href="https://www.flaticon.com/free-animated-icons/miscellaneous"></button>
 
-                <button type="submit" id="editTask2"><img class="editTask" src="/src/images/edit.gif" alt="Edit Task" title="Edit Task"
+                <button type="submit" class="editTask2"><img class="editTask" src="/src/images/edit.gif" alt="Edit Task" title="Edit Task"
                         attribution="https://www.flaticon.com/free-animated-icons/paper"></button>
                 
-                <button type="submit" id="deleteTask2"><img class="deleteTask" src="/src/images/bin.gif"
+                <button type="submit" class="deleteTask2"><img class="deleteTask" src="/src/images/bin.gif"
                         alt="Delete Task" title="Delete Task"
                         attribution="https://www.flaticon.com/free-animated-icons/document"></button>
             </div>
             `
     document.getElementById("tasks").innerHTML += task1;
-    
 }
+
+
 
 //This function will be added to the empty array
 function newTaskArray(taskText) {
@@ -88,6 +91,9 @@ function newTaskArray(taskText) {
 }
 
 
+
+
+// Delete Button
 function taskOperations(usersparam) {
     let taskElements = document.getElementsByClassName("spanLine");
 
@@ -95,64 +101,50 @@ function taskOperations(usersparam) {
         event.remove();
     })
 
-
     for (let i = 0; i < usersparam.length; i++) {
 
         let user1 = appendTask(usersparam[i]);
-        
+
         document.getElementById("tasks").innerHTML + user1;
     }
 
+    taskElements = document.getElementsByClassName("deleteTask2");
 
-    // Update data-index attributes
-    taskElements = document.getElementsByClassName("spanLine");
-
-    Array.from(taskElements).forEach((event, index) => {
-
-        event.setAttribute("data-index", index);
-        event.addEventListener('click', (event) => {
-            removeUser(event, index);
+    Array.from(taskElements).forEach((el) => {
+        el.addEventListener('click', (event) => {
+            removeUser(event);
         });
     });
 };
 
+function removeUser(event, index) {
+
+    const entry = event.target.parentNode.parentNode;
+    taskArray.splice(index, 1);
+    entry.remove();
+}
 
 
 
+//Sort Button
 
+// function sortArrayTasks() {
+//     taskArray.sort((a, b) => {
+//         let titleA = a.title.toLowerCase();
+//         let titleB = b.title.toLowerCase();
 
-
-
-
-
-
-// class DeleteTask {
-//     time = inputDate.value;
-//     title = inputTask.value;
-//     completed = false;
-
-//     constructor(time, title, completed) {
-//         this._time = time;
-//         this._title = title;
-//         this._completed = completed;
-//     }
+//         if (titleA < titleB) {
+//             return -1;
+//         }
+//         if (titleA > titleB) {
+//             return 1;
+//         }
+//         return 0;
+//     })
 // }
 
-// console.log(DeleteTask.task)
+// sortTask.addEventListener('click', () => {
+//     sortArrayTasks();
+//     console.log(taskArray);
+// }); 
 
-
-// function delTaskArray() {
-
-//     for (let i = 0; i < taskArray.length; i++) {
-
-//         if (inputTask.value == DeleteTask.title) {
-//             taskArray.splice(inputTask[i], 1)
-//         }
-//         else {
-//             console.log("Try again!")
-//         }
-//     }
-
-// }
-
-// // deleteTask.addEventListener('click', delTaskArray)
